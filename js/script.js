4e5c243c7cd637de85b8a228b0aa19a6f6a83dfb
Larry H <111525408+larryh12@users.noreply.github.com>
2023-03-28 14:26:30 +0700

    add page router and highlightActiveLink()

---
+++ js/script.js
@@ -0,0 +1,39 @@
+const global = {
+  currentPage: window.location.pathname,
+};
+
+// highlight active link
+const highlightActiveLink = () => {
+  const links = document.querySelectorAll('.nav-link');
+  links.forEach((link) => {
+    link.getAttribute('href') === global.currentPage &&
+      link.classList.add('active');
+  });
+};
+
+// init app
+const init = () => {
+  // multipage router
+  switch (global.currentPage) {
+    case '/':
+    case '/index.html':
+      console.log('Home');
+      break;
+    case '/shows.html':
+      console.log('Shows');
+      break;
+    case '/movie-details.html':
+      console.log('Movie Details');
+      break;
+    case '/tv-details.html':
+      console.log('TV Details');
+      break;
+    case '/search.html':
+      console.log('Search');
+      break;
+  }
+
+  highlightActiveLink();
+};
+
+document.addEventListener('DOMContentLoaded', init);
